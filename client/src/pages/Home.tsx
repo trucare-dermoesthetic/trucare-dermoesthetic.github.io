be32b1f6934bd2119fcc69010fa8788fb6dc9b28
OscarTrujillo <oscartrujillo94@gmail.com>
2025-11-04 13:36:55 +0000

    fix: be name

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -39,18 +39,15 @@ export default function Home() {
     e.preventDefault();
     toast.success("¡Gracias por tu mensaje! Te contactaremos pronto.");
 
-    await fetch(
-      "https://trucare-backend.netlify.app/.netlify/functions/sendEmail",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          to: formData.email,
-          subject: "Welcome!",
-          text: "Hola desde TruCare 🎉",
-        }),
-      }
-    );
+    await fetch("https://trucare-be.netlify.app/.netlify/functions/sendEmail", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        to: formData.email,
+        subject: "Welcome!",
+        text: "Hola desde TruCare 🎉",
+      }),
+    });
     setFormData({ nombre: "", email: "", telefono: "", mensaje: "" });
   };
 
